4695b76f912fc7501eb3d6cb3773903a8a7ad64f
kozhevnikov <kozhevnikov@solidbroker.ru>
2024-07-18 17:28:47 +0300

    Небольшие промежуточные доработки

--- pages/game.vue
+++ pages/game.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 v-sheet(
-  class="mx-auto"
+  class="d-flex"
   width="300"
   v-if="displaySettings.needInitPlayer"
 ) 
@@ -51,10 +51,28 @@ v-sheet(
 div(
   v-if="!displaySettings.needInitPlayer"
 )
+  v-btn(
+    class="mt-4"
+    color="success"
+    block
+    @click="reset"
+  ) Маджонг
+  v-btn(
+    class="mt-4"
+    color="warning"
+    block
+    @click="reset"
+  ) Конг
+  v-btn(
+    class="mt-4"
+    color="error"
+    block
+    @click="reset"
+  ) Закончилась стена
   v-data-table(
     :headers="displaySettings.headers"
-    :items="items"
-    item-key="name"
+    :items="gameState.rounds"
+    item-key="number"
     items-per-page="-1"
     hide-default-footer
   )
@@ -70,7 +88,8 @@ div(
   export default {
     data: () => ({
       gameState: {
-        players: [ '', '', '', '' ]
+        players: [ '', '', '', '' ],
+        rounds: []
       },
       displaySettings: {
         needInitPlayer: true,
@@ -79,12 +98,12 @@ div(
       formInitPlayers: {
         players: [ 'Восток', 'Юг', 'Запад', 'Север'],
         random: false
-      },
-      items: []
+      }
     }),
     methods: {
       initPlayers () {
         this.gameState.players = []
+        this.gameState.rounds = []
         if (this.formInitPlayers.random) {
           const temp = this.formInitPlayers.players.sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5)
           console.log(temp)
@@ -93,13 +112,29 @@ div(
           this.gameState.players.push(...this.formInitPlayers.players)
         }
         this.displaySettings.headers = [
-          { title: "Раунд" },
-          { title: "東 Восток", align: 'center', children: [ { title: this.gameState.players[0], align: 'center' } ] },
-          { title: "南 Юг", align: 'center', children: [ { title: this.gameState.players[1], align: 'center' } ] },
-          { title: "西 Запад", align: 'center', children: [ { title: this.gameState.players[2], align: 'center' } ] },
-          { title: "北 Север", align: 'center', children: [ { title: this.gameState.players[3], align: 'center' } ] }
-        ]        
+          { title: "Раунд", key: "number" },
+          { title: "東 Восток", align: 'center', children: [ { title: this.gameState.players[0], align: 'center', key: 'scores[0].total' } ] },
+          { title: "南 Юг", align: 'center', children: [ { title: this.gameState.players[1], align: 'center', key: 'scores[1].total' } ] },
+          { title: "西 Запад", align: 'center', children: [ { title: this.gameState.players[2], align: 'center', key: 'scores[2].total' } ] },
+          { title: "北 Север", align: 'center', children: [ { title: this.gameState.players[3], align: 'center', key: 'scores[3].total' } ] }
+        ]
+        this.newRound()
         this.displaySettings.needInitPlayer = false
+      },
+      newRound () {
+        const item = {
+          number: this.gameState.rounds.length + 1,
+          scores: [],
+          inGame: [ true, true, true, true ],
+          mahjongNumber: [ -1, -1, -1, -1 ]
+        }
+        for (let i = 0; i < 4; i++) {
+          item.scores.push({
+            total: 0,
+            detail: []
+          })
+        }
+        this.gameState.rounds.push(item)
       },
       reset () {
         this.formInitPlayers.players = [ 'Восток', 'Юг', 'Запад', 'Север']
