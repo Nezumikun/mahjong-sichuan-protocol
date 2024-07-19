63ad7fd7cb0fcee5ec127dc803245adba7038f65
kozhevnikov <kozhevnikov@solidbroker.ru>
2024-07-19 15:52:02 +0300

    Регистрация закрытого конга

--- pages/game.vue
+++ pages/game.vue
@@ -64,7 +64,7 @@ div(
       class="mt-4"
       color="warning"
       block
-      @click="reset"
+      @click="displayKongDialog"
     ) Конг
     v-btn(
       class="mt-4"
@@ -85,11 +85,31 @@ div(
       tr(
         align="center"
       )
-        td {{ item.number }}
-        td {{ item.scores[0].total }}
-        td {{ item.scores[1].total }}
-        td {{ item.scores[2].total }}
-        td {{ item.scores[3].total }}
+        td(rowspan=2) {{ item.number }}
+        template(
+          v-for="(score, index) in item.scores"
+        )
+          td(
+            :class="{ 'bg-yellow-darken-1' : (item.mahjongNumber[index] === 1), 'bg-grey-lighten-2' : (item.mahjongNumber[index] === 2), 'bg-orange-accent-3' : (item.mahjongNumber[index] === 3) }"
+          )
+            template(
+              v-for="(detail, index) in score.detail"
+            )
+              v-chip(
+                :color="detail.score > 0 ? 'success' : 'error'"
+              ) {{ Math.abs(detail.score) }}
+      tr(
+        align="center"
+      )
+        template(
+          v-for="(score, index) in item.scores"
+        )
+          td(
+            :class="{ 'bg-yellow-darken-1' : (item.mahjongNumber[index] === 1), 'bg-grey-lighten-2' : (item.mahjongNumber[index] === 2), 'bg-orange-accent-3' : (item.mahjongNumber[index] === 3) }"
+          )
+            v-chip(
+              :color="score.total > 0 ? 'success' : score.total < 0 ? 'error': ''"
+            ) {{ score.total }}
         <!--td {{ item  }}-->
   v-btn(
     class="mt-4"
@@ -131,6 +151,51 @@ v-dialog(
         variant="tonal"
         @click="displaySettings.mahjong = false"
       )
+
+v-dialog(
+  width="auto"
+  min-width=300
+  v-model="displaySettings.kong"
+)
+  v-card
+    v-card-title Конг
+    v-card-text
+      v-form
+        v-select(
+          label="Кто объявил конг?"
+          :items="displaySettings.items"
+          item-title="key"
+          item-value="value"
+          v-model="displaySettings.kongPlayer"
+          @update:modelValue="updateKongSourceList"
+        )
+        v-radio-group(
+          v-model="displaySettings.kongFrom"
+          v-if="displaySettings.kongPlayer !== null"
+        )
+          v-radio(label="Закрытый конг" value=-2)
+          v-radio(label="Доставленный конг" value=-1)
+          template(
+            v-for="item in displaySettings.kongItems"
+          )
+            v-radio(
+              :value="item.value"
+              :label="'с ' + item.key"
+            )
+    v-card-actions
+      v-btn(
+        text="Ok"
+        color="success"
+        variant="tonal"
+        :disabled="displaySettings.kongFrom === 0"
+        @click="kong"
+      )
+      v-btn(
+        text="Отмена"
+        color="error"
+        variant="tonal"
+        @click="displaySettings.kong = false"
+      )
 </template>
 
 <script>
@@ -147,7 +212,11 @@ v-dialog(
         roundInProggress: false,
         mahjong: false,
         mahjongPlayer: -1,
+        kong: false,
+        kongPlayer: null,
+        kongFrom: 0,
         items: [],
+        kongItems: [],
         headers: []
       },
       formInitPlayers: {
@@ -165,7 +234,6 @@ v-dialog(
         }
         if (this.formInitPlayers.random) {
           const temp = this.formInitPlayers.players.sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5)
-          console.log(temp)
           this.gameState.players.push(...temp)
         } else {
           this.gameState.players.push(...this.formInitPlayers.players)
@@ -185,7 +253,8 @@ v-dialog(
           number: this.gameState.rounds.length + 1,
           scores: [],
           inGame: [ true, true, true, true ],
-          mahjongNumber: [ -1, -1, -1, -1 ]
+          mahjongNumber: [ -1, -1, -1, -1 ],
+          events: []
         }
         for (let i = 0; i < 4; i++) {
           item.scores.push({
@@ -202,12 +271,12 @@ v-dialog(
         this.formInitPlayers.random = false
         this.displaySettings.needInitPlayer = true
       },
-      displayMahjongDialog() {
-        const currentRound = this.gameState.rounds.at(0)
-        this.displaySettings.items = []
+      getActivePlayers(excludePlayer) {
+        const currentRound = this.gameState.rounds[0]
+        const items = []
         for (let key in this.gameState.players) {
-          if (currentRound.inGame[key]) {
-            this.displaySettings.items.push(
+          if (currentRound.inGame[key] && (key !== excludePlayer)) {
+            items.push(
               {
                 key: this.gameState.players[key],
                 value: key
@@ -215,19 +284,56 @@ v-dialog(
             )
           }
         }
+        return items
+      },
+      displayMahjongDialog() {
+        this.displaySettings.items = this.getActivePlayers()
         this.displaySettings.mahjongPlayer = -1
         this.displaySettings.mahjong = true
+      },
+      displayKongDialog() {
+        this.displaySettings.items = []
+        this.displaySettings.items = this.getActivePlayers()
+        this.displaySettings.kongPlayer = null
+        this.displaySettings.kong = true
+        this.displaySettings.kongFrom = 0
+        this.displaySettings.kongItems = []
+      },
+      updateKongSourceList() {
+        this.displaySettings.kongItems = this.getActivePlayers(this.displaySettings.kongPlayer)
       },
       mahjong () {
         this.displaySettings.mahjong = false
         this.gameState.mahjongCount++
         const winner = this.displaySettings.mahjongPlayer
-        const currentRound = this.gameState.rounds.at(0)
+        const currentRound = this.gameState.rounds[0]
         currentRound.inGame[winner] = false
         currentRound.mahjongNumber[winner] = this.gameState.mahjongCount
         if (this.gameState.mahjongCount === 3) {
           this.displaySettings.roundInProggress = false
           this.newRound()
+        }
+      },
+      kong () {
+        this.displaySettings.kong = false
+        const winner = this.displaySettings.kongPlayer
+        const currentRound = this.gameState.rounds[0]
+        if (this.displaySettings.kongFrom === "-2") {
+          const loosers = this.getActivePlayers(winner)
+          currentRound.scores[winner].total += 2 * loosers.length
+          currentRound.scores[winner].detail.push({
+             score: 2 * loosers.length,
+             type: 'kong',
+             source: loosers
+          })
+          for (let looser of loosers) {
+            currentRound.scores[looser.value].total -= 2
+            currentRound.scores[looser.value].detail.push({
+              score: -2,
+              type: 'kong',
+              destination: winner
+            })
+          }
         }
       },
       endOfWall () {
